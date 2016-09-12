aa06164dc60a7f49b3f21cc43ebeb0434df80c81
Ahmad Game <ahmad.game@lundalogik.se>
2016-09-12 17:53:49 +0200

    Some more code

--- ElasticLabs/Program.cs
+++ ElasticLabs/Program.cs
@@ -1,26 +1,84 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Nest;
 using Newtonsoft.Json;
 
 namespace ElasticLabs
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             var client = new ElasticClient(new Uri("http://192.168.99.100:9200"));
-            var response = client.Search<SearchDocument>(s => s.MatchAll());
 
-            foreach (var hit in response.Hits)
+            var searchResponse = client.YouKnowForSearch();
+            WriteResults(searchResponse.Documents);
+
+            searchResponse = client.SearchInAnIndex();
+            WriteResults(searchResponse.Documents);
+
+            searchResponse = client.SearchInMultipleIndices();
+            WriteResults(searchResponse.Documents);
+
+            searchResponse = client.SearchForType();
+            WriteResults(searchResponse.Documents);
+
+        }
+
+        private static ISearchResponse<SearchDocument> YouKnowForSearch(this IElasticClient client)
+        {
+            return client.Search<SearchDocument>(searchDescriptor => searchDescriptor.MatchAll());
+            
+            // Can also be written more verbosely like this:
+            /*
+            var searchDescriptor = new SearchDescriptor<SearchDocument>();
+            searchDescriptor.MatchAll();
+            return client.Search<SearchDocument>(searchDescriptor);
+            */
+        }
+
+        private static ISearchResponse<SearchDocument> SearchInAnIndex(this IElasticClient client)
+        {
+            return client.Search<SearchDocument>(
+                searchDescriptor => searchDescriptor
+                    .MatchAll()
+                    .Index("pase"));
+        }
+        
+        private static ISearchResponse<SearchDocument> SearchInMultipleIndices(this IElasticClient client)
+        {
+            return client.Search<SearchDocument>(
+                searchDescriptor => searchDescriptor
+                    .MatchAll()
+                    .Index(Indices.Index("pase", "go1")));
+        }
+
+        private static ISearchResponse<SearchDocument> SearchForType(this IElasticClient client)
+        {
+            return client.Search<SearchDocument>(s => s
+                .MatchAll()
+                .Index(Indices.Index("pase", "go1"))
+                .Type("Organization"));
+        }
+
+
+
+        private static void WriteResults(IEnumerable<SearchDocument> documents)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(documents, Formatting.Indented));
+            Console.WriteLine("==========================================================================");
+            Console.WriteLine("Continue? (type 'q' to quit)");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'q')
             {
-                Console.WriteLine(JsonConvert.SerializeObject(hit.Source));
+                Environment.Exit(0);
             }
-
-            Console.Read();
         }
     }
 
-    internal class SearchDocument 
+    public class SearchDocument 
     {
         [JsonProperty("heading")]
         public string Heading { get; set; }
